237dfb346198d76fb0597e59a59c97ab7e3a451b
lubingbing <heroermon@sina.com>
2017-02-27 13:23:51 +0800

    fix

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>i am ratings</div>
+  <div>I am ratings</div>
 </template>
 
 <script type="text/ecmascript-6">
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>i am seller</div>
+  <div>I am seller</div>
 </template>
 
 <script type="text/ecmascript-6">
