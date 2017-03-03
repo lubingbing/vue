91046852205b1ccadb825774c743662910bd95fe
lubingbing <heroermon@sina.com>
2017-03-03 12:38:48 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -45,14 +45,14 @@
   @import "./common/stylus/mixin.styl"
 
   .tab
-    display flex
+    display: flex
     width: 100%
     height: 40px
     line-height: 40px
     border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
       flex: 1
-      text-align center
+      text-align: center
       & > a
         display: block
         font-size: 14px
